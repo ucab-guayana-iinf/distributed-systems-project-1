293488706cae2c78eeca9e35588d7d97e1e4064a
Juan Marcos <juanmarcos.vera12@gmail.com>
2021-06-01 00:23:48 -0400

    Add TCP client implementation

---
+++ src/clients/tcp/tcp-client.go
@@ -0,0 +1,39 @@
+// package tcpClient
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
+func main() {
+	address := "localhost:2020"
+	fmt.Println("[TCP Client]: Starting")
+
+	c, err := net.Dial("tcp", address)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for {
+		// Read user input
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print(">> ")
+		text, _ := reader.ReadString('\n')
+
+		// Send input to the server
+		fmt.Fprintf(c, text+"\n")
+
+		// Read messages from the server
+		message, _ := bufio.NewReader(c).ReadString('\n')
+		fmt.Print("Server: " + message)
+		if strings.TrimSpace(string(text)) == "STOP" {
+			fmt.Println("TCP client exiting...")
+			return
+		}
+	}
+}
