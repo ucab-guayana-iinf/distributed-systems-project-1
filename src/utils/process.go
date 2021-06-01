f09f21b38147326a28eb59e2ba3e46c173628cc6
Juan Marcos <juanmarcos.vera12@gmail.com>
2021-06-01 00:23:00 -0400

    Create process test

--- src/utils/process.go
+++ src/utils/process.go
@@ -1,14 +1,19 @@
 package utils
 
 import (
+	"bufio"
+	"fmt"
+	"io"
 	"log"
 	"os/exec"
+	"runtime"
 	"strconv"
+	"time"
 )
 
 // TODO: use to validate count min/max
-const MaxInt = int(MaxUint >> 1) 
-const MinInt = -MaxInt - 1
+// const MaxInt = int(MaxUint >> 1)
+// const MinInt = -MaxInt - 1
 
 func StartProcess() {
 	log.Printf("Haha business")
@@ -25,4 +30,51 @@ func StartProcess() {
 
 func IntToString(n int) string {
 	return strconv.Itoa(n)
+}
+
+func CreateProcessTest() {
+	var cmd string
+
+	if runtime.GOOS == "windows" {
+		cmd = "cmd.exe"
+	} else {
+		cmd = "/bin/bash"
+	}
+
+	// Returns a pointer to the process
+	proc := exec.Command(cmd)
+
+	// Get process' standard input
+	stdin, err := proc.StdinPipe()
+	if err != nil {
+		panic(err.Error())
+	}
+	defer stdin.Close()
+
+	// Get process' standard output
+	stdout, _ := proc.StdoutPipe()
+	defer stdout.Close()
+
+	// Function that reads the outputs of the process and prints them
+	go func() {
+		scanner := bufio.NewScanner(stdout)
+		for scanner.Scan() {
+			fmt.Println("From scanner:", scanner.Text())
+		}
+	}()
+
+	// Start the cmd process
+	err = proc.Start()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// Write to the process' console and execute a command
+	_, err = io.WriteString(stdin, "ping google.com\n")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	time.Sleep(time.Second * 20)
+	proc.Process.Kill()
 }
