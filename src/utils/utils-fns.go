9745d11d71a660f00a64d4a842e59b66f0329f2e
Angel Rodríguez <angelmanuelrs@gmail.com>
2021-06-19 11:48:18 -0400

    Remove unused functions

--- src/utils/utils-fns.go
+++ src/utils/utils-fns.go
@@ -1,14 +1,7 @@
 package utils
 
 import (
-	"bufio"
-	"fmt"
-	"io"
-	"log"
-	"os/exec"
-	"runtime"
 	"strconv"
-	"time"
 )
 
 type OperationsStruct struct {
@@ -26,19 +19,6 @@ const DECREMENT = "Decrementar"
 const RESTART = "Reiniciar"
 const GET_COUNT = "Imprimir"
 
-func StartProcess() {
-	log.Printf("Haha business")
-
-	cmd := exec.Command("sleep", "5")
-	err := cmd.Start()
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("Waiting for command to finish...")
-	err = cmd.Wait()
-	log.Printf("Command finished with error: %v", err)
-}
-
 func IntToString(n int) string {
 	return strconv.Itoa(n)
 }
@@ -49,52 +29,4 @@ func StringToInt(s string) int {
 		panic(err)
 	}
 	return res
-}
-
-func CreateProcessTest() {
-	var cmd string
-
-	if runtime.GOOS == "windows" {
-		cmd = "cmd.exe"
-	} else {
-		cmd = "/bin/bash"
-	}
-
-	// Returns a pointer to the process
-	proc := exec.Command(cmd)
-
-	// Get process' standard input
-	stdin, err := proc.StdinPipe()
-	if err != nil {
-		panic(err.Error())
-	}
-	defer stdin.Close()
-
-	// Get process' standard output
-	stdout, _ := proc.StdoutPipe()
-	defer stdout.Close()
-
-	// Function that reads the outputs of the process and prints them
-	go func() {
-		scanner := bufio.NewScanner(stdout)
-		for scanner.Scan() {
-			fmt.Println("From scanner:", scanner.Text())
-		}
-	}()
-
-	// Start the cmd process
-	err = proc.Start()
-	if err != nil {
-		panic(err.Error())
-	}
-
-	// Write to the process' console and execute a command
-	// _, err = io.WriteString(stdin, "ping google.com\n")
-	_, err = io.WriteString(stdin, "go run main.go\n")
-	if err != nil {
-		panic(err.Error())
-	}
-
-	time.Sleep(time.Second * 20)
-	proc.Process.Kill()
 }
